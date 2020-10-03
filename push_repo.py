4f27528ce28459bfc928d6a3f48eeb51ea3d0f2b
bavelee <bavelee@foxmail.com>
2020-10-03 21:30:28 +0800

    Update:20201003

--- push_repo.py
+++ push_repo.py
@@ -2,20 +2,25 @@
 import os, json, codecs, time
 data = []
 os.chdir('sound')
-files = os.listdir('.')
-files.sort()
-for file in files:
-		if os.path.isfile(file) and file.endswith('mp3') or file.endswith('ogg'):
-			name = os.path.splitext(file)[0].split('_')[-1]
-			url = "https://imbavelee.coding.net/p/GiaoTone/d/giao-tone-server/git/raw/master/sound/" + file
-			item = {'name' : name, 'url' : url}
-			data.append(item)
+categories = os.listdir('.')
+categories.sort()
+
+for category in categories:
+	category_name = os.path.splitext(category)[0].split('_')[-1]
+	sounds = os.listdir('./' + category)
+	sounds.sort()
+	for sound_file in sounds:
+		name = os.path.splitext(sound_file)[0].split('_')[-1]
+		url = "https://imbavelee.coding.net/p/GiaoTone/d/giao-tone-server/git/raw/master/sound/" + category + '/' + sound_file
+		item = {'name' : category_name + ' ' + name, 'url' : url, 'category' : category_name}
+		data.append(item)
 
 list = {"list" : data}
 
 file_name = '../data.json'
 with codecs.open(file_name, 'w', encoding='utf-8') as file_obj:
 	file_obj.write(json.dumps(list).encode('utf-8').decode("unicode_escape"))
+
 
 os.chdir('..')
 t = time.strftime("%Y%m%d", time.localtime())
